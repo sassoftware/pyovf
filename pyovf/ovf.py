b53070b54cf27907d14dfbf86ba57dfc939372db
Erik Troan <http://issues.rpath.com>
2009-01-09 19:58:19 -0500

    fleshed out classes for ovf generation

--- pyovf/ovf.py
+++ pyovf/ovf.py
@@ -68,17 +68,78 @@ class DiskSection(object):
     def __init__(self):
         self.ovf_Disk = []
 
+class Network(object):
+
+    _xobj = xobj.XObjMetadata(
+            attributes = { "ovf_id" : str,
+                           "ovf_name" : xobj.XID } )
+
+    def __init__(self, id, name):
+        self.ovf_id = id
+        self.ovf_name = name
+        if name is not None:
+            self.ovf_name = name
+
 class NetworkSection(object):
 
     ovf_Network = [ object ]
 
+    def __init__(self):
+        self.ovf_Network = []
+
+class pProperty(object):
+
+    pass
+
+class Property(pProperty):
+
+    _xobj = xobj.XObjMetadata(attributes = { 'ovf_key' : str,
+                                             'ovf_type' : str } )
+
+    def __init__(self, key, type):
+        self.ovf_key = key
+        self.ovf_type = type
+
+class pProduct(object):
+
+    ovf_info = str
+    ovf_productVersion = str
+    ovf_Property = [ pProperty ]
+
+class Product(pProduct):
+
+    def addProperty(self, p):
+        self.ovf_Property.append(p)
+
+    def __init__(self, info, version = None):
+        self.ovf_Property = []
+        self.ovf_info = info
+        if version:
+            self.ovf_productVersion = version
+
 class ProductSection(object):
 
-    ovf_Property = [ object ]
+    ovf_Product = [ pProduct ]
+
+    def __init__(self):
+        self.ovf_Product = []
+
+class pVirtualSystem(object):
+
+    _xobj = xobj.XObjMetadata(attributes = { 'ovf_id' : str,
+                                             'ovf_info' : str } )
+
+    ovf_ProductSection = [ ProductSection ]
 
 class VirtualSystem(object):
 
-    ovf_ProductSection = [ ProductSection ]
+    def addProduct(self, p):
+        self.ovf_ProductSection.append(p)
+
+    def __init__(self, id, info):
+        self.ovf_ProductSection = []
+        self.ovf_id = id
+        self.ovf_info = info
 
 class ReferencesSection(object):
 
@@ -92,20 +153,27 @@ class Ovf(object):
     ovf_References = ReferencesSection
     ovf_DiskSection = DiskSection
     ovf_NetworkSection = NetworkSection
-    ovf_VirtualSystem = [ VirtualSystem ]
+    ovf_VirtualSystem = [ { 'VirtualSystem' : VirtualSystem,
+                            'VirtualSystemCollection' : object } ]
 
     def addDisk(self, d):
         if d.ovf_fileRef not in self.ovf_References.ovf_File:
             self.addFileReference(d.ovf_fileRef)
         self.ovf_DiskSection.ovf_Disk.append(d)
 
+    def addNetwork(self, n):
+        self.ovf_NetworkSection.ovf_Network.append(n)
+
     def addFileReference(self, r):
         self.ovf_References.ovf_File.append(r)
+
+    def addSystem(self, vs):
+        self.ovf_VirtualSystem.append(vs)
 
     def toxml(self):
         schemaPath = os.path.join(os.path.dirname(__file__),
                                   "schemas/ovf-envelope.xsd")
-        return self._doc.toxml()
+        return self._doc.toxml(nsmap = self._doc.nameSpaceMap)
 
 class NewOvf(Ovf):
 
@@ -116,6 +184,8 @@ class NewOvf(Ovf):
 
         self.ovf_References = ReferencesSection()
         self.ovf_DiskSection = DiskSection()
+        self.ovf_NetworkSection = NetworkSection()
+        self.ovf_VirtualSystem = []
 
 class OvfDocument(xobj.Document):
 
