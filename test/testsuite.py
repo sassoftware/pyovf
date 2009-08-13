9a5664d1d44070fcabba6d02a260c68f00acda5a
Whitney Battestilli <http://issues.rpath.com/>
2009-08-13 10:14:43 -0400

    major testsuite updates

--- test/testsuite.py
+++ test/testsuite.py
@@ -3,22 +3,16 @@
 import os
 import sys
 import unittest
-
 import bootstrap
-from testrunner import testhelp
-from testrunner import resources, testhandler
 
 _setupPath = None
+_individual = False
 
-def setupPath(pathName):
-    path = testhelp.getPath(pathName)
-    os.environ[pathName] = path
-    for p in path.split(':'):
-        if not os.path.isdir(p):
-            print '%s %s does not exist' % (pathName, path)
-            sys.exit(1)
-    testhelp.insertPath(path, updatePythonPath=True)
+def isIndividual():
+    global _individual
+    return _individual
 
+from testrunner import pathManager
 
 def setup():
     """
@@ -29,45 +23,60 @@ def setup():
     if _setupPath:
         return _setupPath
 
-    path = setupPath('XOBJ_PATH')
-    path = setupPath('PYOVF_PATH')
-
-    from testrunner import testSetup
-    testSetup.setup()
+    pathManager.addExecPath('CONARY_PATH')
+    pathManager.addExecPath('XOBJ_PATH')
+    pathManager.addExecPath('PYOVF_PATH')
+    pyovfTestPath = pathManager.addExecPath('PYOVF_TEST_PATH')
+    pathManager.addExecPath('TEST_PATH',path=pyovfTestPath)
 
     from conary.lib import util
     sys.excepthook = util.genExcepthook(True, catchSIGUSR1=False)
 
-    testhelp._conaryDir = resources.conaryDir
     _setupPath = True
 
 
+
+
+
 def main(argv=None, individual=True):
-    cfg = resources.cfg
-    cfg.isIndividual = individual
+    global _individual
+    _individual = individual
 
     setup()
-
-    cfg.cleanTestDirs = not individual
-    cfg.coverageExclusions = ['scripts/.*', 'epdb.py', 'stackutil.py',
-                              'test/.*']
-    cfg.coverageDirs = [ os.environ['PYOVF_PATH'] ]
-
-    if argv is None:
-        argv = list(sys.argv)
-    topdir = testhelp.getTestPath()
-    if topdir not in sys.path:
-        sys.path.insert(0, topdir)
-    cwd = os.getcwd()
-    if cwd != topdir and cwd not in sys.path:
-        sys.path.insert(0, cwd)
 
     from conary.lib import util
     from conary.lib import coveragehook
     sys.excepthook = util.genExcepthook(True, catchSIGUSR1=False)
 
-    handler = testhandler.TestSuiteHandler(cfg, resources)
+    from testrunner import testhandler,testhelp
+    def getCoverageDirs():
+        return [ pathManager.getPath('PYOVF_PATH') ]
+
+    def getCoverageExclusions():
+        return ['scripts/.*', 'epdb.py', 'stackutil.py',
+                'test/.*']
+    def sortTests(tests):
+        order = {'smoketest': 0, 
+                 'unit_test' :1,
+                 'functionaltest':2}
+        maxNum = len(order)
+        tests = [ (test, test.index('test')) for test in tests]
+        tests = sorted((order.get(test[:index+4], maxNum), test)
+                       for (test, index) in tests)
+        tests = [ x[1] for x in tests ]
+        return tests
+    handlerClass = testhelp.getHandlerClass(testhelp.ConaryTestSuite,
+                                            getCoverageDirs,
+                                            getCoverageExclusions,
+                                            sortTests)
+    handler = handlerClass(individual=individual, topdir=pathManager.getPath('PYOVF_TEST_PATH'),
+                           testPath=pathManager.getPath('PYOVF_TEST_PATH'),
+                           conaryDir=pathManager.getPath('CONARY_PATH'))
+
     print "This process PID:", os.getpid()
+
+    if argv is None:
+        argv = list(sys.argv)
     results = handler.main(argv)
     if results is None:
         sys.exit(0)
