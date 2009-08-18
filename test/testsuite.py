ba2f8025114b7220d81ec07a9deab2e660e57cf6
Michael Tharp <mtharp@rpath.com>
2009-08-18 10:53:51 -0400

    Testsuite fixes.

--- test/testsuite.py
+++ test/testsuite.py
@@ -5,74 +5,35 @@ import sys
 import unittest
 
 import bootstrap
-from testrunner import testhelp
-from testrunner import resources, testhandler
 
-_setupPath = None
+from testrunner import pathManager, testhelp
 
-def setupPath(pathName):
-    path = testhelp.getPath(pathName)
-    os.environ[pathName] = path
-    for p in path.split(':'):
-        if not os.path.isdir(p):
-            print '%s %s does not exist' % (pathName, path)
-            sys.exit(1)
-    testhelp.insertPath(path, updatePythonPath=True)
+
+EXCLUDED_PATHS = ['scripts/.*', 'test/.*']
 
 
 def setup():
-    """
-    Setup initializes variables must be initialized before the testsuite
-    can be run.  Generally this means setting up and determining paths.
-    """
-    global _setupPath
-    if _setupPath:
-        return _setupPath
-
-    path = setupPath('XOBJ_PATH')
-    path = setupPath('PYOVF_PATH')
-
-    from testrunner import testSetup
-    testSetup.setup()
-
-    from conary.lib import util
-    sys.excepthook = util.genExcepthook(True, catchSIGUSR1=False)
-
-    testhelp._conaryDir = resources.conaryDir
-    _setupPath = True
+    pathManager.addExecPath('XOBJ_PATH')
+    path = pathManager.addExecPath('PYOVF_PATH')
+    print path
+    pathManager.addResourcePath('TEST_PATH', path + '/test')
 
 
 def main(argv=None, individual=True):
-    cfg = resources.cfg
-    cfg.isIndividual = individual
-
-    setup()
-
-    cfg.cleanTestDirs = not individual
-    cfg.coverageExclusions = ['scripts/.*', 'epdb.py', 'stackutil.py',
-                              'test/.*']
-    cfg.coverageDirs = [ os.environ['PYOVF_PATH'] ]
-
     if argv is None:
         argv = list(sys.argv)
-    topdir = testhelp.getTestPath()
-    if topdir not in sys.path:
-        sys.path.insert(0, topdir)
-    cwd = os.getcwd()
-    if cwd != topdir and cwd not in sys.path:
-        sys.path.insert(0, cwd)
 
-    from conary.lib import util
     from conary.lib import coveragehook
-    sys.excepthook = util.genExcepthook(True, catchSIGUSR1=False)
 
-    handler = testhandler.TestSuiteHandler(cfg, resources)
-    print "This process PID:", os.getpid()
+    handlerClass = testhelp.getHandlerClass(testhelp.ConaryTestSuite,
+            lambda handler, environ: os.getenv('PYOVF_PATH') + '/pyovf',
+            lambda handler, environ: EXCLUDED_PATHS)
+
+    handler = handlerClass(individual=individual)
     results = handler.main(argv)
-    if results is None:
-        sys.exit(0)
-    sys.exit(not results.wasSuccessful())
+    return results.getExitCode()
 
 
 if __name__ == '__main__':
-    main(sys.argv, individual=False)
+    setup()
+    sys.exit(main(sys.argv, individual=False))
